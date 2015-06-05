6c2f048563d607c4d852d0d0cb40bef39919dd96
JongChan Choi <disjukr@naver.com>
2015-06-06 01:39:58 +0900

    nup

--- examples/tokenize.rs
+++ examples/tokenize.rs
@@ -1,15 +1,24 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::BufReader;
+
 extern crate fbx;
+use fbx::parser::ascii::*;
 
 fn main() {
-    use fbx::parser::ascii::*;
-    let tokens = tokenize("FBXHeaderExtension:  { FBXHeaderVersion: 1003");
+    let fixture = "fixtures/2013_ASCII/Cinema4D.fbx";
+    let mut source = String::new();
+    let mut fbx_file_reader = BufReader::new(File::open(fixture).unwrap());
+    fbx_file_reader.read_to_string(&mut source);
+    let tokens = tokenize(&source);
+    println!("{}\n", source);
     for token in tokens.iter() {
-        println!("{}", match token.token_type {
+        println!("{}\t{}", match token.token_type {
             TokenType::OpenBracket => "{",
             TokenType::CloseBracket => "}",
             TokenType::Data => "DATA",
             TokenType::Comma => ",",
             TokenType::Key => "KEY",
-        });
+        }, token.contents);
     }
 }
